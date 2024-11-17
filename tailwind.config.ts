5990a78c7d911e4d683578edde79acd2697ac021
mennaElbadry70 <mennatallah701@gmail.com>
2024-11-17 19:08:19 +0200

    intial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,15 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
+     "./app/**/*.{js,ts,jsx,tsx}",
+     "./src/**/*.{html,js}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      colors: {},
     },
   },
   plugins: [],
